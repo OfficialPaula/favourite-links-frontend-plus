81d27665dbee6f54af69551345fc3b149167d827
OfficialPaula <95579427+OfficialPaula@users.noreply.github.com>
2023-05-25 01:09:55 -0400

    Implement Dark/light mode + TypeScript/Node updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import LinkContainer from './components/LinkContainer'
+import DarkMode from './components/DarkMode.tsx';
 
 function App() {
   return (
     <div className="App">
       <LinkContainer />
+      <DarkMode />
     </div>
   )
 }
